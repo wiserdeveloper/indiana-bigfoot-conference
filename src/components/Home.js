d1a333441ba6fbc2a5f42aec57b8790d675f5111
wiserdeveloper <wiserdeveloper@gmail.com>
2023-08-01 19:34:30 -0400

    desktop homepage finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,10 +12,16 @@ const Home = () => {
                <div className="headerImg">
                     <img src={HeaderImg} alt="" />
                </div>
-               <div className="event-info">
-                    <h1>September 27th - 28th, 2024</h1>
-                    <h1 id="event-location">The Seasons Lodge, Nashville, Indiana</h1>
-                    <NavLink className='register-btn' to='/attend'>Register to Attend Now!</NavLink>
+               <div className="hp-body">
+                    <div className="event-info">
+                         <h1>September 27th - 28th, 2024</h1>
+                         <h1 id="event-location">The Seasons Lodge, Nashville, Indiana</h1>
+                         <NavLink className='register-btn' to='/attend'>Register to Attend Now!</NavLink>
+                    </div>
+                    <div className="event-desc">
+                         <h1>Indiana's 1st Annual Bigfoot Conference</h1>
+                         <p>Get ready to mark your calendars and join us for an unforgettable experience at Indiana's highly anticipated Bigfoot Conference taking place from September 27th to September 28th, 2024. Nestled in the captivating realm of Nashville, Indiana, renowned for its legendary Bigfoot sightings, this exceptional event is an absolute must-attend for all enthusiasts and curious minds alike.</p>
+                    </div>
                </div>
           </div>
      )
