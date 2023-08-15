6a06d31472e8a33e7fd734e2488d5a9fe18f94f1
wiserdeveloper <wiserdeveloper@gmail.com>
2023-08-15 14:02:38 -0400

    fixed image, added text

--- client/src/pages/Location.js
+++ client/src/pages/Location.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import './location.css';
-import nashvilleImg from "../components/img/nashville-in.jpg"
+import desktopImg from "../components/img/nashville-in.jpg"
+import mobileImg from '../components/img/mobile-nashville.jpg'
 
 const Location = () => {
 
@@ -12,23 +13,28 @@ const Location = () => {
                     <span className="location-topper">Location</span>
                     <h2 className="location-title">Nashville, Indiana</h2>
                </div>
-
+               
+               <div className="location-flex-container">
                <div className="location-img-container">
                <picture className="location-background">
                     {/* <!--Mobile Image--> */}
-                    <source media="(max-width: 600px)" srcSet={nashvilleImg} />
+                    <source media="(max-width: 600px)" srcSet={mobileImg} />
                     {/* <!--Tablet and above Image--> */}
-                    <source media="(min-width: 601px)" srcSet={nashvilleImg} />
+                    <source media="(min-width: 601px)" srcSet={desktopImg} />
                     <img
                     loading="lazy"
                     decoding="async"
-                    srcSet={nashvilleImg}
-                    width='700'
-                    height='400'
+                    srcSet={desktopImg}
                     alt="nashville, indiana"
                     aria-hidden="true"
                     />
                </picture>
+               </div>
+
+               <div className="location-text-container">
+               <p>Nestled within the rolling hills of southern Indiana, Nashville exudes a unique charm that captivates the soul of every traveler. This picturesque town is an artistic haven, where quaint streets are adorned with galleries, studios, and boutiques, showcasing the works of local artisans. The air is filled with the sweet melodies of live music, offering a soothing backdrop as you explore the scenic trails, lush forests, and serene waterways that surround the area. With its warm community spirit and a welcoming atmosphere, Nashville, Indiana, invites you to indulge in its creative spirit, relish in outdoor adventures, and discover a hidden gem that promises unforgettable memories and a true sense of connection with both art and nature.</p>
+               <h1>Come & Explore Nashville!</h1>
+               </div>
                </div>
 
           </div>
