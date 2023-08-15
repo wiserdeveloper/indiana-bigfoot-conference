a214598a250ba6e5c44061b5e9dbd37730ecefb5
wiserdeveloper <wiserdeveloper@gmail.com>
2023-08-14 20:03:31 -0400

    added to location pg, having trouble sizing img

--- client/src/pages/Location.js
+++ client/src/pages/Location.js
@@ -1,11 +1,39 @@
 import React from "react";
 
 import './location.css';
+import nashvilleImg from "../components/img/nashville-in.jpg"
 
 const Location = () => {
-     <div>
-          <h1>Location Page</h1>
-     </div>
+
+     return (
+     <section id="location">
+          <div className="location-container">
+               <div className="location-content">
+                    <span className="location-topper">Location</span>
+                    <h2 className="location-title">Nashville, Indiana</h2>
+               </div>
+
+               <div className="location-img-container">
+               <picture className="location-background">
+                    {/* <!--Mobile Image--> */}
+                    <source media="(max-width: 600px)" srcSet={nashvilleImg} />
+                    {/* <!--Tablet and above Image--> */}
+                    <source media="(min-width: 601px)" srcSet={nashvilleImg} />
+                    <img
+                    loading="lazy"
+                    decoding="async"
+                    srcSet={nashvilleImg}
+                    width='700'
+                    height='400'
+                    alt="nashville, indiana"
+                    aria-hidden="true"
+                    />
+               </picture>
+               </div>
+
+          </div>
+     </section>
+     )
 
 }
 
