c51332bd496dce091dbc8e738064bf19af23b4af
wiserdeveloper <wiserdeveloper@gmail.com>
2023-08-26 14:42:44 -0400

    installed Stripe and added API keys

--- client/src/pages/Tickets.js
+++ client/src/pages/Tickets.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY)
+
 const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
 const secretKey = process.env.REACT_APP_STRIPE_SECRET_KEY;
 
