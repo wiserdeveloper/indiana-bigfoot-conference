3a4089583f161b32e34f7e1c3423e0003bd2b19d
wiserdeveloper <wiserdeveloper@gmail.com>
2024-09-19 10:32:21 -0400

    added schedule to homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -33,6 +33,115 @@ const Home = () => {
             for all enthusiasts and curious minds alike.</h3>
           </div>
 
+          <div className="schedule-container">
+            <h1>Schedule</h1>
+            <div className="friday-container">
+              <h3 className="friday-date">Friday, September 27th <br /> 4pm - 8pm</h3>
+              <h3>Town Hall</h3>
+              <div className="schedule list-container">
+                <div className="schedule-list">
+                  <p>4pm - 7pm</p>
+                  <p>Doors open for Vendor sales</p>
+                </div>
+                <div className="schedule-list">
+                  <p>4pm - 7pm</p>
+                  <p>Pre-registration for Saturday's Conference</p>
+                </div>
+                <div className="schedule-list">
+                  <p>4pm - 7pm</p>
+                  <p>Raffle tickets for Ebike available for purchase</p>
+                </div>
+                <div className="schedule-list">
+                  <p>5pm - 7pm</p>
+                  <p>Town Hall</p>
+                </div>
+                <div className="schedule-list">
+                  <p>8pm</p>
+                  <p>Venue closed</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="saturday-container">
+              <h3>Saturday, September 28th <br /> 8am - 8pm</h3>
+              <h3>Speaker Event</h3>
+              <div className="schedule list-container">
+                <div className="schedule-list">
+                  <p>8am</p>
+                  <p>Doors open for event & vendor sales</p>
+                </div>
+                <div className="schedule-list">
+                  <p>8am - 5pm</p>
+                  <p>Registration for speaker event</p>
+                </div>
+                <div className="schedule-list">
+                  <p>8am - 5pm</p>
+                  <p>Ebike raffle tickets available for purchase</p>
+                </div>
+                <div className="schedule-list">
+                  <p>9am - 10am</p>
+                  <p>LeRoy Nail -- IBRO Host of Event</p>
+                </div>
+                <div className="schedule-list">
+                  <p>10am - 10:15am</p>
+                  <p>Break</p>
+                </div>
+                <div className="schedule-list">
+                  <p>10:15am - 11:15am</p>
+                  <p>Packman Paranormal</p>
+                </div>
+                <div className="schedule-list">
+                  <p>11:15am - 11:30am</p>
+                  <p>Break</p>
+                </div>
+                <div className="schedule-list">
+                  <p>11:30am - 12:30pm</p>
+                  <p>Charlie Raymond</p>
+                </div>
+                <div className="schedule-list">
+                  <p>12:30pm - 2pm</p>
+                  <p>Lunch break</p>
+                </div>
+                <div className="schedule-list">
+                  <p>2pm - 3pm</p>
+                  <p>Squatchman (Greg Yost)</p>
+                </div>
+                <div className="schedule-list">
+                  <p>3pm - 3:15pm</p>
+                  <p>Break</p>
+                </div>
+                <div className="schedule-list">
+                  <p>3:15pm - 4:15pm</p>
+                  <p>Amy Bue</p>
+                </div>
+                <div className="schedule-list">
+                  <p>4:15pm - 4:30pm</p>
+                  <p>Break</p>
+                </div>
+                <div className="schedule-list">
+                  <p>4:30pm - 5:30pm</p>
+                  <p>Marc DeWerth</p>
+                </div>
+                <div className="schedule-list">
+                  <p>5:30pm - 5:45pm</p>
+                  <p>Break</p>
+                </div>
+                <div className="schedule-list">
+                  <p>5:45pm - 6:45pm</p>
+                  <p>Adam Davies</p>
+                </div>
+                <div className="schedule-list">
+                  <p>7pm - 7:30pm</p>
+                  <p>LeRoy Nail -- closing & ebike winner announced</p>
+                </div>
+                <div className="schedule-list">
+                  <p>8pm</p>
+                  <p>Venue Closed</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="flyer-container">
             {/* <picture className="con-flyer">
               <img 
